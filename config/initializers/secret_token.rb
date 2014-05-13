edb4e0bac5bf39b15fed4fc2c0851528e72ec7cc
Leslie Viljoen <leslie.viljoen@localist.co.nz>
2014-05-13 20:29:41 +1200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Critterserv::Application.config.secret_key_base = '9c989281d420a2c7782cd1f60975bd4f288c8e05e5c3b2c128d0d2a253e85d1ba3fa87009dd1078933726775f8dd1860ddc22d6eda47e43416a62173fb8bb02e'
